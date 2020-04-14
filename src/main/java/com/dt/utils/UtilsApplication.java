50bdbf2a70687d687122406986746ce6fb96688a
dt <279964527@qq.com>
2020-04-14 11:10:48 +0800

    消费

--- src/main/java/com/dt/utils/UtilsApplication.java
+++ src/main/java/com/dt/utils/UtilsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.annotation.EnableKafka;
+
 /**
  * @Author jiangyao
  * @Date 2019/6/27 9:35
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.dt.utils"})
 @EnableCaching //开启注解
+@EnableKafka
 public class UtilsApplication {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(UtilsApplication.class);
---
+++ src/main/java/com/dt/utils/kafka/KafkaLogsConsumer.java
@@ -0,0 +1,45 @@
+package com.dt.utils.kafka;
+
+import com.dt.utils.util.RedisUtil;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @Author jiangyao
+ * @Date 2020/4/14 9:08
+ **/
+@Component
+public class KafkaLogsConsumer {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(KafkaLogsConsumer.class);
+
+    private static final String TOPIC_PREFIX = "LOG_TOPIC";
+    private static final String TEST_TOPIC = "TEST_TOPIC";
+    private static final String TOPIC_DATE_PATTERN = "yyyy-MM-dd";
+
+    @Autowired
+    RedisUtil redisUtil;
+
+    @KafkaListener(topics = TOPIC_PREFIX)
+    public void listenMsg (ConsumerRecord<?,String> record) {
+        String value = record.value();
+        LOGGER.info("ConsumerMsg====>>"+value);
+        String format = DateFormatUtils.format(new Date(), TOPIC_DATE_PATTERN);
+        redisUtil.lSet(KafkaLogsConsumer.TOPIC_PREFIX+"_"+format,value);
+    }
+
+    @KafkaListener(topics = TEST_TOPIC)
+    public void testMsg (ConsumerRecord<?,String> record) {
+        String value = record.value();
+        LOGGER.info("ConsumerMsg====>>"+value);
+    }
+
+}
