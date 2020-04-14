f17c2d8041cf39d153cb88226ce6c86bf66ec912
dt <279964527@qq.com>
2020-04-14 09:03:29 +0800

    连接池

--- src/main/java/com/dt/utils/config/RedisConfig.java
+++ src/main/java/com/dt/utils/config/RedisConfig.java
@@ -3,13 +3,21 @@ package com.dt.utils.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 单机
@@ -19,17 +27,73 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.cache.nodes}")
+    private String nodes;
+//    @Value("${spring.redis.cache.host}")
+//    private String host;
+    @Value("${spring.redis.cache.password}")
+    private String password;
+    @Value("${spring.redis.cache.maxIdle}")
+    private Integer maxIdle;
+    @Value("${spring.redis.cache.minIdle}")
+    private Integer minIdle;
+    @Value("${spring.redis.cache.maxTotal}")
+    private Integer maxTotal;
+    @Value("${spring.redis.cache.maxWaitMillis}")
+    private Long maxWaitMillis;
+
+    @Bean
+    LettuceConnectionFactory lettuceConnectionFactory() {
+        // 连接池配置
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setMaxIdle(maxIdle == null ? 8 : maxIdle);
+        poolConfig.setMinIdle(minIdle == null ? 1 : minIdle);
+        poolConfig.setMaxTotal(maxTotal == null ? 8 : maxTotal);
+        poolConfig.setMaxWaitMillis(maxWaitMillis == null ? 5000L : maxWaitMillis);
+        LettucePoolingClientConfiguration lettucePoolingClientConfiguration = LettucePoolingClientConfiguration.builder()
+                .poolConfig(poolConfig)
+                .build();
+        // 单机redis
+//        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+//        redisConfig.setHostName(host==null||"".equals(host)?"localhost":host.split(":")[0]);
+//        redisConfig.setPort(Integer.valueOf(host==null||"".equals(host)?"6379":host.split(":")[1]));
+//        if (password != null && !"".equals(password)) {
+//            redisConfig.setPassword(password);
+//        }
+
+        // 哨兵redis
+        // RedisSentinelConfiguration redisConfig = new RedisSentinelConfiguration();
+
+        // 集群redis
+        RedisClusterConfiguration redisConfig = new RedisClusterConfiguration();
+        Set<RedisNode> nodeses = new HashSet<>();
+        String[] hostses = nodes.split(",");
+        for (String h : hostses) {
+            h = h.replaceAll("\\s", "").replaceAll("\n", "");
+            if (!"".equals(h)) {
+                String host = h.split(":")[0];
+                int port = Integer.valueOf(h.split(":")[1]);
+                nodeses.add(new RedisNode(host, port));
+            }
+        }
+        redisConfig.setClusterNodes(nodeses);
+        // 跨集群执行命令时要遵循的最大重定向数量
+        redisConfig.setMaxRedirects(3);
+        redisConfig.setPassword(password);
+
+        return new LettuceConnectionFactory(redisConfig, lettucePoolingClientConfiguration);
+    }
     /**
      * retemplate相关配置
-     * @param factory
+     * @param lettuceConnectionFactory
      * @return
      */
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
 
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         // 配置连接工厂
-        template.setConnectionFactory(factory);
+        template.setConnectionFactory(lettuceConnectionFactory);
 
         //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
         Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
